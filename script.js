2eeaf1a63839d43112623c92b458710b23719a50
Arfan rasyid <221810116@stis.ac.id>
2021-10-08 08:06:52 +0700

    predict

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 
 // var init
-let useData=[], chartLabel=[], chartData=[], slope=[],sData=data.length;
+let chartLabel=[], chartData=[], slope=[],sData=data.length;
 let sX=0, sY=0, sX2=0, sY2=0, sXY=0, x=0;
 let tr,a,b,itemData,lastModel;
 const durasi = 1000; //ms
 const table = document.getElementById('table');
-const modelEq = document.querySelector('#model .eq').innerHTML;
 let tempTable = "<tr><td class='{c}'>{i}</td><td>{tgl}</td><td>{ihsg}</td></tr>";
 const bulan = [[1,'Januari'],[2,'Februari'],[3,'Maret'],[4,'April'],[5,'Mei'],[6,'Juni'],[7,'Juli'],[8,'Agustus'],[9,'September'],[10,'Oktober'],[11,'November'],[12,'Desember']];
 const tahun = Array(2021-1990+1).fill().map((x,i)=>i+1990);
@@ -22,9 +21,11 @@ bulan.forEach((val)=>{
     tr = tr.replace('{val}',val[0]).replace('{bln}',val[1])
     control.sb.insertAdjacentHTML( 'beforeend', tr )
     control.eb.insertAdjacentHTML( 'beforeend', tr )
+    document.getElementById('mbulan').insertAdjacentHTML( 'beforeend', tr )
 })
 tahun.forEach((val)=>{
     tr = `<option>{th}</option>`;
+    document.getElementById('mTahun').insertAdjacentHTML( 'beforeend', tr.replace('{th}',val+32) )
     tr = tr.replace('{th}',val)
     control.st.insertAdjacentHTML( 'beforeend', tr )
     control.et.insertAdjacentHTML( 'beforeend', tr )
@@ -79,9 +80,9 @@ var myChart = new Chart(ctx, {
                     y:data[i].close,
                     x2:Math.pow(x,2),
                     y2:Math.pow(data[i].close,2),
-                    xy:(x)*data[i].close
+                    xy:(x)*data[i].close,
+                    tgl:data[i].tgl
                 };
-                useData.push(itemData)
                 sX+=itemData.x; sY+=itemData.y; sX2+=itemData.x2; sY2+=itemData.y2; sXY+=itemData.xy;
                 
                 chartLabel.push(data[i].date)
@@ -92,9 +93,9 @@ var myChart = new Chart(ctx, {
                 if(i>(durasi*60/sData)) myChart.update();
 
                 lastModel = model(itemData.x+1,sX,sY,sX2);
-                if(lastModel.fb>0){tr = "Y="+lastModel.fa+'+'+lastModel.fb+"X";} 
-                else {tr = 'Y='+lastModel.fa+lastModel.fb+'X';}
-                if(i%100==0 || i==4) document.querySelector('#model .eq').innerHTML = tr;
+                if(lastModel.fb>0){tr = "Model: Y="+lastModel.fa+'+'+lastModel.fb+"X";} 
+                else {tr = 'Model: Y='+lastModel.fa+lastModel.fb+'X';}
+                if(i%100==0 || i==4) document.querySelector('#model').innerHTML = tr;
                 if (tableinit) {
                     tr = tempTable.replace('{c}','xdata c-'+data[i].tgl).replace('{i}',x);
                     tr = tr.replace('{tgl}',data[i].date).replace('{ihsg}',data[i].close)
@@ -112,16 +113,20 @@ var myChart = new Chart(ctx, {
             }
         },durasi/sData);
     }
-    draw({dari:data[0].tgl,sampai:data[sData-1].tgl})
-    // draw({dari:1523232000,sampai:1633599811})
+    
+    if (true) {
+        draw({dari:data[0].tgl,sampai:data[sData-1].tgl})
+    } else {
+        draw({dari:1523232000,sampai:1633599811})
+    }
 
     function trendline(model,n) {
         // console.log({model,n,itemData})
         myChart.data.datasets[1].data = []
         slope=[]
-        if(model.fb>0){tr = "\\(Y="+model.fa+'+'+model.fb+"X\\)";} 
-        else {tr = "\\(Y="+model.fa+model.fb+"X\\)";}
-        document.querySelector('#model .eq').innerHTML = tr;
+        if(model.fb>0){tr = "Model: \\(Y="+model.fa+'+'+model.fb+"X\\)";} 
+        else {tr = "Model: \\(Y="+model.fa+model.fb+"X\\)";}
+        document.querySelector('#model').innerHTML = tr;
         MathJax.Hub.Queue(["Typeset",MathJax.Hub])
 
         for (let i = 0; i < n; i++) {
@@ -159,6 +164,22 @@ var myChart = new Chart(ctx, {
         myChart.update();
         sX=0; sY=0; sX2=0; sY2=0; sXY=0;
         draw(time,false)
+    })
+
+    document.getElementById('predict').addEventListener('click',()=>{
+        let Xmodel = document.getElementById('mTahun').value+'/';
+        Xmodel += document.getElementById('mbulan').value+'/1';
+        Xmodel = new Date(Xmodel).getTime()/1000 - itemData.tgl;
+        Xmodel = Math.ceil(Xmodel/7/24/60/60) + itemData.x;
+
+        tr = lastModel.b*(Xmodel)+lastModel.a;
+        if(lastModel.fb>0){
+            tr = "Prediksi: \\("+lastModel.fa+'+'+lastModel.fb+"("+Xmodel+")="+tr.toFixed(2)+"\\)";
+        } else {
+            tr = "Prediksi: \\("+lastModel.fa+lastModel.fb+"("+Xmodel+")="+tr.toFixed(2)+"\\)";
+        }
+        document.querySelector('#model').innerHTML = tr;
+        MathJax.Hub.Queue(["Typeset",MathJax.Hub])
     })
 
 })();
